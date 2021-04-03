55e72440c1e718eeae48398376a210ff49202e96
divij <divij.827@gmail.com>
2021-04-03 11:59:12 +0530

    wip login page

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    login
+    {{token}}
   </div>
 </template>
 
 <script>
+import {mapGetters} from 'vuex';
 export default {
-
+  computed: {
+    ...mapGetters({
+      token: 'auth/token'
+    })
+  },
+  mounted() {
+    // this.$store.dispatch('auth/setToken', 'tmkc');
+  }
 }
 </script>
 
---
+++ store/auth.js
@@ -0,0 +1,29 @@
+export const state = () => ({
+  token: undefined
+});
+
+export const actions = {
+  getToken({commit}) {
+    const token = document.cookie.split(';').find(cookie => cookie.includes('token'))?.trim().split('=')[1];
+    commit('SET_TOKEN', token);
+  },
+  setToken({dispatch}, val) {
+    const expiry = new Date();
+    expiry.setDate(expiry.getDate() + 6);
+    document.cookie = `token=${val};expires=${expiry.toUTCString()}`;
+    dispatch('getToken');
+  },
+  revokeToken() {
+    document.cookie= 'token=;max-age=0';  
+  }
+};
+
+export const mutations = {
+  SET_TOKEN(state,val) {
+    state.token = val;
+  }
+};
+
+export const getters = {
+  token: ({token}) => token
+}
